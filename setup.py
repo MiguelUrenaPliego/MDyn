c79c2d281f649d2672a848becdee483a1fddc000
MiguelUrenaPliego <94168783+MiguelUrenaPliego@users.noreply.github.com>
2025-02-06 19:24:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=[
         "numpy>=2.0.0",
         "matplotlib>=3.0.0",
-        "scipy>=1.0.0",
+        "scipy>=0.18.1",
         "imageio>=2.0.0",
     ],
     classifiers=[
